fcfc58d8b66d9861e1b3465c230db06be092e60b
benhooper21 <benjamin.hooper@uqconnect.edu.au>
2018-09-26 16:46:39 +1000

    added app page - forgot to before

---
+++ src/features/app/AppComponent.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class AppComponent extends Component {
+    render() {
+        return(
+            <div>App Page Works!</div>
+        );
+    }
+
+}
+
+export default AppComponent;
