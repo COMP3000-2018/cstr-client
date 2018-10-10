310e09873cafaac1995ccc62400881a71b79afa4
Chris Wilkinson <chris@chriswilkinson.co>
2018-10-10 16:13:17 +1000

    real links to apps

--- src/Components/App/AppComponent.js
+++ src/Components/App/AppComponent.js
@@ -12,15 +12,15 @@ class AppComponent extends Component {
                     </tr>
                     <tr>
                         <td>Glasgow Coma Scale</td>
-                        <td><a href="https://www.google.com">https://www.mdcalc.com/glasgow-coma-scale-score-gcs</a></td>
+                        <td><a href="https://www.mdcalc.com/glasgow-coma-scale-score-gcs">https://www.mdcalc.com/glasgow-coma-scale-score-gcs</a></td>
                     </tr>
                     <tr>
                         <td>Lund-Browder Chart</td>
-                        <td><a href="https://www.google.com">http://www.vicburns.org.au/wp-content/uploads/2016/06/poster4-paediatric-burns-assessment-ruler.pdf</a></td>
+                        <td><a href="http://www.vicburns.org.au/wp-content/uploads/2016/06/poster4-paediatric-burns-assessment-ruler.pd">http://www.vicburns.org.au/wp-content/uploads/2016/06/poster4-paediatric-burns-assessment-ruler.pdf</a></td>
                     </tr>
                     <tr>
                         <td>ANZBA Guidelines</td>
-                        <td><a href="https://www.google.com">http://anzba.org.au/care/allied-health-guidelines/</a></td>
+                        <td><a href="http://anzba.org.au/care/allied-health-guidelines/">http://anzba.org.au/care/allied-health-guidelines/</a></td>
                     </tr>
                 </table>
             </div>
