7e52bcd3a1a5e9264b96796a991417304548366b
Chris Wilkinson <chris@chriswilkinson.co>
2018-10-10 17:24:35 +1000

    Less generic radiobutton names

--- src/Components/GCS/GCSComponent.js
+++ src/Components/GCS/GCSComponent.js
@@ -14,76 +14,76 @@ class GCSComponent extends Component {
                     <tr>
                         <td>
                             <div>
-                                <input type="radio" id="huey" name="drone" value="huey" checked />
-                                <label for="huey">Spontaneously (+4)</label>
+                                <input type="radio" id="E4" name="E4" value="E4" checked />
+                                <label for="E4">Spontaneously (+4)</label>
                             </div>
                             <div>
-                                <input type="radio" id="dewey" name="drone" value="dewey" />
-                                <label for="dewey">To Verbal Command (+3)</label>
+                                <input type="radio" id="E3" name="E3" value="E3" />
+                                <label for="E3">To Verbal Command (+3)</label>
                             </div>
                             <div>
-                                <input type="radio" id="louie" name="drone" value="louie" />
-                                <label for="louie">To Pain (+2)</label>
+                                <input type="radio" id="E2" name="E2" value="E2" />
+                                <label for="E2">To Pain (+2)</label>
                             </div>
                             <div>
-                                <input type="radio" id="louie" name="drone" value="louie" />
-                                <label for="louie">No Eye Opening (+1)</label>
+                                <input type="radio" id="E1" name="E1" value="E1" />
+                                <label for="E1">No Eye Opening (+1)</label>
                             </div>
                             <div>
-                                <input type="radio" id="louie" name="drone" value="louie" />
-                                <label for="louie">Not assessable (+1c)</label>
-                            </div>
-                        </td>
-                        <td>
-                            <div>
-                                <input type="radio" id="huey" name="drone" value="huey" checked />
-                                <label for="huey">Oriented (+5)</label>
-                            </div>
-                            <div>
-                                <input type="radio" id="dewey" name="drone" value="dewey" />
-                                <label for="dewey">Confused (+4)</label>
-                            </div>
-                            <div>
-                                <input type="radio" id="louie" name="drone" value="louie" />
-                                <label for="louie">Innapropriate Words (+3)</label>
-                            </div>
-                            <div>
-                                <input type="radio" id="louie" name="drone" value="louie" />
-                                <label for="louie">Incomprehensible Sounds (+2)</label>
-                            </div>
-                            <div>
-                                <input type="radio" id="louie" name="drone" value="louie" />
-                                <label for="louie">No Verbal Response (+1)</label>
-                            </div>
-                            <div>
-                                <input type="radio" id="louie" name="drone" value="louie" />
-                                <label for="louie">Incubated (+1t)</label>
+                                <input type="radio" id="E1c" name="E1c" value="E1c" />
+                                <label for="E1c">Not assessable (+1c)</label>
                             </div>
                         </td>
                         <td>
                             <div>
-                                <input type="radio" id="huey" name="drone" value="huey" checked />
-                                <label for="huey">Obeys Commands (+6)</label>
+                                <input type="radio" id="V5" name="V5" value="V5" checked />
+                                <label for="V5">Oriented (+5)</label>
                             </div>
                             <div>
-                                <input type="radio" id="dewey" name="drone" value="dewey" />
-                                <label for="dewey">Localises Pain (+5)</label>
+                                <input type="radio" id="V4" name="V4" value="V4" />
+                                <label for="V4">Confused (+4)</label>
                             </div>
                             <div>
-                                <input type="radio" id="louie" name="drone" value="louie" />
-                                <label for="louie">Withdrawal From Pain (+4)</label>
+                                <input type="radio" id="V3" name="V3" value="V3" />
+                                <label for="V3">Innapropriate Words (+3)</label>
                             </div>
                             <div>
-                                <input type="radio" id="louie" name="drone" value="louie" />
-                                <label for="louie">Flexion to Pain (+3)</label>
+                                <input type="radio" id="V2" name="V2" value="V2" />
+                                <label for="V2">Incomprehensible Sounds (+2)</label>
                             </div>
                             <div>
-                                <input type="radio" id="louie" name="drone" value="louie" />
-                                <label for="louie">Extension to Pain (+2)</label>
+                                <input type="radio" id="V1" name="V1" value="V1" />
+                                <label for="V1">No Verbal Response (+1)</label>
                             </div>
                             <div>
-                                <input type="radio" id="louie" name="drone" value="louie" />
-                                <label for="louie">No Motor Response (+1)</label>
+                                <input type="radio" id="V1t" name="V1t" value="V1t" />
+                                <label for="V1t">Incubated (+1t)</label>
+                            </div>
+                        </td>
+                        <td>
+                            <div>
+                                <input type="radio" id="M6" name="M6" value="M6" checked />
+                                <label for="M6">Obeys Commands (+6)</label>
+                            </div>
+                            <div>
+                                <input type="radio" id="M5" name="M5" value="M5" />
+                                <label for="M5">Localises Pain (+5)</label>
+                            </div>
+                            <div>
+                                <input type="radio" id="M4" name="M4" value="M4" />
+                                <label for="M4">Withdrawal From Pain (+4)</label>
+                            </div>
+                            <div>
+                                <input type="radio" id="M3" name="M3" value="M3" />
+                                <label for="M3">Flexion to Pain (+3)</label>
+                            </div>
+                            <div>
+                                <input type="radio" id="M2" name="M2" value="M2" />
+                                <label for="M2">Extension to Pain (+2)</label>
+                            </div>
+                            <div>
+                                <input type="radio" id="M1" name="M1" value="M1" />
+                                <label for="M1">No Motor Response (+1)</label>
                             </div>
                         </td>
                     </tr>
