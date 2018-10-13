08f5dd93bf511a2f43d1135c7c35173851893e80
Chris Wilkinson <chris@chriswilkinson.co>
2018-10-13 13:20:07 +1000

    Finished ANZBA page

--- src/Components/ANZBA/ANZBACompohent.js
+++ src/Components/ANZBA/ANZBACompohent.js
@@ -4,7 +4,25 @@ class ANZBAComponent extends Component {
     render() {
         return(
             <div>
-                <td onClick={()=> window.open("https://anzba.org.au/care/referral-criteria/", "_blank")}><h1>ANZBA refferal criteria</h1></td>
+                <h1>ANZBA Referral Criteria</h1>
+            
+                <i>Refer patient if any of the following criteria match:</i>
+                <ul className="referralCriteria">
+                    <li>Burns greater than 10% Total Body Surface Area (TBSA)</li>
+                    <li>Burns greater than 5% TBSA in children</li>
+                    <li>Full Thickness burns greater than 5% TBSA</li>
+                    <li>Burns of Special Areas – Face, Hands, Feet, Genitalia, Perineum, Major Joints and circumferential limb or chest burns</li>
+                    <li>Burns with inhalation injury</li>
+                    <li>Electrical burns</li>
+                    <li>Chemical burns</li>
+                    <li>Burns with pre-existing illness</li>
+                    <li>Burns associated with major trauma</li>
+                    <li>Burns at the extremes of age – young children and the elderly.</li>
+                    <li>Burn injury in pregnant women</li>
+                    <li>Non-accidental burns</li>
+                </ul>
+        
+                <i>Source: <a href="https://anzba.org.au/care/referral-criteria/">https://anzba.org.au/care/referral-criteria/</a></i>
             </div>
 
         );
