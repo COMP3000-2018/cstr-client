6c856a2c4f27c135d56e6d60d2e057812f230ac5
Chris Wilkinson <chris@chriswilkinson.co>
2018-10-13 14:37:47 +1000

    Finished Discussion page

--- src/Components/Diccussion/DiccussionCompohent.js
+++ src/Components/Diccussion/DiccussionCompohent.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import Dropdown from './dropdown.js';
+import Collapse from '../Accordion/Collapse.jsx';
+import Panel from '../Accordion/Panel.jsx';
+require('../Accordion/collapse.css');
 
 class DiccussionComponent extends Component {
     constructor(){
@@ -39,13 +41,32 @@ class DiccussionComponent extends Component {
 
     render() {
         return(
-            <div>
+            <div>    
                 <h1>Diccussion Starters</h1>
-                <Dropdown
-            title="Select Points"
-            list={this.state.datasets}
-            content = {this.state.x}
-          />
+                <Collapse accordion={true}>
+                    <Panel header="Discussion Points 1" headerClass="discussion-panels">
+                        <ol class="discussion-points">
+                            <li>Is there any significance in his previous burn?</li>
+                            <li>How are burns assessed with respect to thickness and total body surface area?</li>
+                            <li>What is the pathophysiology of the various types of burns?</li>
+                            <li>Does Nathan require additional protection against tetanus?</li>
+                            <li>Why is important to obtain IV access?</li>
+                            <li>What other management might you consider? Who else might you consult?</li>
+                            <li>When should a patient be transferred to a specialised burns unit?</li>
+                            <li>What is the purpose of the gladwrap? What other options are there for dressing a burn?</li>
+                        </ol>
+                    </Panel>
+                    <Panel header="Discussion Points 2" headerClass="discussion-panels">
+                           <ol class="discussion-points">
+                                <li>What are the most common sources of burns in adults? What about children?</li>
+                                <li>How can the severity of a burn be judged?</li>
+                                <li>What are the immediate and longer term complications of a burn?</li>
+                                <li>What is appropriate first aid in a burned patient?</li>
+                                <li>What elements of the history are especially important here?</li>
+                                <li>Are any population groups more at risk of burn injury?</li>
+                            </ol> 
+                    </Panel>
+                  </Collapse>
             </div>
         );
     }
