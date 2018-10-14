8456f162fc77b024b47ebf8769c25c8be721ca96
Chris Wilkinson <chris@chriswilkinson.co>
2018-10-14 13:23:01 +1000

    new burn chart, adjusted gradients

--- src/Components/Charts/BarChart.js
+++ src/Components/Charts/BarChart.js
@@ -20,16 +20,17 @@ class BarChart extends Component {
       choice: this.props.pick,
       vale: this.props.data,
       colours: [{color:<linearGradient id="CoolGradient" x1="0" x2="1" y1="0" y2="0">
-      <stop offset="10%" stopColor="red"/>
-      <stop offset="100%" stopColor="green" />
+      <stop offset="0%" stopColor="#fb3d32"/>
+      <stop offset="50%" stopColor="#ffdc39"/>
+      <stop offset="100%" stopColor="#3ab451" />
     </linearGradient>},
-    {color:<linearGradient id="CoolGradient" x1="0" x2="1" y1="0" y2="0">
-    <stop offset="15%" stopColor="#fb3d32"/>
-    <start offset="15%" stopColor="#ffdc39" />
-    <stop offset="65%" stopColor="#ffdc39" />
-    <start offset="65%" stopColor="#fb3d32" />
-    <stop offset="100%" stopColor="#fb3d32"/>
-  </linearGradient>}]
+        {color:<linearGradient id="CoolGradient" x1="0" x2="1" y1="0" y2="0">
+        <stop offset="0%" stopColor="#fb3d32"/>
+        <stop offset="33%" stopColor="#fb3d32"/>
+        <stop offset="33%" stopColor="#ffdc39" />
+        <stop offset="66%" stopColor="#ffdc39"/>
+        <stop offset="66%" stopColor="#fb3d32"/>
+    </linearGradient>}]
     }
   }
 
