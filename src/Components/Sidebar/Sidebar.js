1a6835a25e6f9628007519d0796b981c65303f4a
Chris Wilkinson <chris@chriswilkinson.co>
2018-10-15 10:55:48 +1000

    Better title

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -7,7 +7,7 @@ class Sidebar extends Component {
         const { items, currentlySelected, changeCurrentlySelected } = this.props;
 
         return (<div className="sidebar">
-            <div className="sidebar-header">COMP3000</div>
+            <div className="sidebar-header">MEDI7212</div>
             <div className="sidebar-items">
                 {items.map(item => <SidebarItem onClick={() => changeCurrentlySelected(item.name)} selected={currentlySelected === item.name} name={item.name} icon={item.icon} category={item.category} />)}
             </div>
