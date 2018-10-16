494f2fd45fd3f49966b75d3b18204bf29cb44118
Chris Wilkinson <chris@chriswilkinson.co>
2018-10-16 13:32:58 +1000

    fixed typos, swapped discussion points

--- src/Components/Discussion/DiscussionCompohent.js
+++ src/Components/Discussion/DiscussionCompohent.js
@@ -6,37 +6,6 @@ require('../Accordion/collapse.css');
 class DiscussionComponent extends Component {
     constructor(){
         super()
-        this.state = {
-        x: "",
-          datasets: [
-            {
-                id: 0,
-                title: 'Discussion Points 1',
-                selected: false,
-                key: 'Discussion Points',
-                x: <ul><li>Is there any significance in his previous burn?</li>
-                <li>How are burns assessed with respect to thickness and total body surface area?</li>
-                <li>What is the pathophysiology of the various types of burns?</li>
-                <li>Does Nathan require additional protection against tetanus?</li>
-                <li>Why is important to obtain IV access?</li>
-                <li>What other management might you consider? Who else might you consult?</li>
-                <li>When should a patient be transferred to a specialised burns unit?</li>
-                <li>What is the purpose of the gladwrap? What other options are there for dressing a burn?</li></ul>
-            },
-            {
-              id: 1,
-              title: 'Discussion Points 2',
-              selected: false,
-              key: 'Discussion Points',
-              x: <ul><li>What are the most common sources of burns in adults? What about children?</li>
-              <li>How can the severity of a burn be judged?</li>
-              <li>What are the immediate and longer term complications of a burn?</li>
-              <li>What is appropriate first aid in a burned patient?</li>
-              <li>What elements of the history are especially important here?</li>
-              <li>Are any population groups more at risk of burn injury?</li></ul>
-            }
-          ]
-        }
       }
 
     render() {
@@ -45,6 +14,16 @@ class DiscussionComponent extends Component {
                 <h1>Discussion Starters</h1>
                 <Collapse accordion={true}>
                     <Panel header="Discussion Points 1" headerClass="discussion-panels">
+                            <ol class="discussion-points">
+                                <li>What are the most common sources of burns in adults? What about children?</li>
+                                <li>How can the severity of a burn be judged?</li>
+                                <li>What are the immediate and longer term complications of a burn?</li>
+                                <li>What is appropriate first aid in a burned patient?</li>
+                                <li>What elements of the history are especially important here?</li>
+                                <li>Are any population groups more at risk of burn injury?</li>
+                            </ol>
+                    </Panel>
+                    <Panel header="Discussion Points 2" headerClass="discussion-panels">
                         <ol class="discussion-points">
                             <li>Is there any significance in his previous burn?</li>
                             <li>How are burns assessed with respect to thickness and total body surface area?</li>
@@ -55,16 +34,6 @@ class DiscussionComponent extends Component {
                             <li>When should a patient be transferred to a specialised burns unit?</li>
                             <li>What is the purpose of the gladwrap? What other options are there for dressing a burn?</li>
                         </ol>
-                    </Panel>
-                    <Panel header="Discussion Points 2" headerClass="discussion-panels">
-                           <ol class="discussion-points">
-                                <li>What are the most common sources of burns in adults? What about children?</li>
-                                <li>How can the severity of a burn be judged?</li>
-                                <li>What are the immediate and longer term complications of a burn?</li>
-                                <li>What is appropriate first aid in a burned patient?</li>
-                                <li>What elements of the history are especially important here?</li>
-                                <li>Are any population groups more at risk of burn injury?</li>
-                            </ol> 
                     </Panel>
                   </Collapse>
             </div>
