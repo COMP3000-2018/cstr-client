00589004288ed1708873fa53a21858d652635626
rex s <Rexsw98@hotmail.com>
2018-10-24 15:59:40 +1000

    minor chart fix

--- src/Components/Charts/ChartComponent.js
+++ src/Components/Charts/ChartComponent.js
@@ -25,13 +25,13 @@ class ChartComponent extends Component {
             if (unlocked.BloodPressure)
                 Charts.push(<Panel header="Blood Pressure" headerClass="discussion-panels" visibility><BloodChart/></Panel>);
             if (unlocked.HeartRate)
-                Charts.push(<Panel header="Heart Rate" headerClass="discussion-panels"><BarSeries x={[80,160]} title= {'Heart Rate'} pick={1} data={140}/></Panel>);
+                Charts.push(<Panel header="Heart Rate" headerClass="discussion-panels"><BarSeries x={[80,161]} title= {'Heart Rate'} pick={1} data={140}/></Panel>);
             if (unlocked.OxygenSaturation)
-                Charts.push(<Panel header="Oxygen Saturation" headerClass="discussion-panels"><BarSeries x={[85,100]} title= {'Oxygen saturation'} pick={0} data={100}/></Panel>);
+                Charts.push(<Panel header="Oxygen Saturation" headerClass="discussion-panels"><BarSeries x={[85,101]} title= {'Oxygen saturation'} pick={0} data={100}/></Panel>);
             if (unlocked.Temperature)
-                Charts.push(<Panel header="Temperature" headerClass="discussion-panels"><BarSeries x={[34,42]} title= {'Temperature'} pick={1} data={35}/></Panel>);
+                Charts.push(<Panel header="Temperature" headerClass="discussion-panels"><BarSeries x={[34,43]} title= {'Temperature'} pick={1} data={35}/></Panel>);
             if (unlocked.Weight)
-                Charts.push(<Panel header="Weight" headerClass="discussion-panels"><BarSeries x={[50,100]} title= {'Weight'} pick={1}  data={75}/></Panel>);
+                Charts.push(<Panel header="Weight" headerClass="discussion-panels"><BarSeries x={[50,101]} title= {'Weight'} pick={1}  data={75}/></Panel>);
             
             // Wrap in necessary parent
             Charts = <Collapse accordion={true}>{Charts}</Collapse>;
